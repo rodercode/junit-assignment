e6b08a0b39166bf423562c3c3e41a6a118625a0c
rodercode <rodersnow@gmail.com>
2023-09-01 11:36:40 +0200

    refactor fahrenheit to celsius method and create a new method that make a value one decimal place

--- src/main/java/org/example/Convert.java
+++ src/main/java/org/example/Convert.java
@@ -3,8 +3,11 @@ package org.example;
 public class Convert {
 
     public double fahrenheitToCelsius(double fahrenheit){
-        double value = (fahrenheit -32)/1.8;
-        double roundedValue = Math.floor(value * 10) / 10.0;;
-        return roundedValue;
+        double celsius = (fahrenheit -32)/1.8;
+        return roundToOneDecimalPlace(celsius);
+    }
+
+    public double roundToOneDecimalPlace(double value){
+        return Math.floor(value * 10) / 10.0;
     }
 }
