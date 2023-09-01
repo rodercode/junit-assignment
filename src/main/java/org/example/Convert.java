37a5c96711d2ac5f5a2fd81285bcaa27d15c30c3
rodercode <rodersnow@gmail.com>
2023-09-01 11:28:17 +0200

    create a test check 52f to 11c and modify fahrenheitToCelsius method

--- src/main/java/org/example/Convert.java
+++ src/main/java/org/example/Convert.java
@@ -3,6 +3,8 @@ package org.example;
 public class Convert {
 
     public double fahrenheitToCelsius(double fahrenheit){
-        return (fahrenheit-32)/1.8;
+        double value = (fahrenheit -32)/1.8;
+        double roundedValue = Math.floor(value * 10) / 10.0;;
+        return roundedValue;
     }
 }
--- src/test/java/org/example/ConvertTest.java
+++ src/test/java/org/example/ConvertTest.java
@@ -11,14 +11,11 @@ class ConvertTest {
     // A system should be able to convert F to C
     // user should enter F68
     // return C20
-
     Convert convert;
-
     @BeforeEach
     public void setup(){
         convert = new Convert();
     }
-
     @Test
     @DisplayName("Should convert 68F to 20C")
     public void shouldReturnCelsiusTwenty() {
@@ -26,7 +23,14 @@ class ConvertTest {
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("Should convert 32F to 0C ")
     public void shouldReturnCelsiusZero(){
+        assertEquals(0, convert.fahrenheitToCelsius(32));
+    }
+
+    @Test
+    @DisplayName("Should convert 52F to 11C")
+    public void shouldReturnADecimalValue(){
+        assertEquals(11.1,convert.fahrenheitToCelsius(52));
     }
 }
